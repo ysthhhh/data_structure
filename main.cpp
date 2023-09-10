bcbc3ea98a44e0c47c57d7b5d7f4f0943328eeaa
2209040039 <2304238596@qq.com>
2023-09-10 06:42:56 +0000

    test 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello World\n");
-    return 0;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>un_map;
+        vector<int>res;
+        for(int i=0;i<nums.size();i++){
+            if(un_map.find(target-nums[i])!=un_map.end()){
+                res.push_back(i);
+                res.push_back(un_map[target-nums[i]]);
+            }
+            un_map[nums[i]] = i;
+        }
+        return res;
+    }
+};
